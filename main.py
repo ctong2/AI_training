7390f88ea194d497fb7a199822318cd716d26f59
ctong2 <103022336+ctong2@users.noreply.github.com>
2025-07-22 14:23:16 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -9,56 +9,46 @@ st.title("File Analyzer")
 model_name = "gpt-4.1"
 
 # display the password text input 
-api_key = st.sidebar.text_input("API_KEY", type = "password")
+api_key = st.sidebar.text_input("API KEY", type = "password")
 
 if api_key:
     try:
         client = OpenAI(api_key = api_key)
-
         # initialize the session state if not exists
-
-        
-        # display all history contents based on the role 
-
-        
-        # display the chat input with placeholder, and the types of files accepted
         if "messages" not in st.session_state:
             st.session_state.messages = []
             st.session_state.display_content = []
+        # display all history contents based on the role 
+        for message in st.session_state.display_content:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"], unsafe_allow_html = True)
+        
+        # display the chat input with placeholder, and the types of files accepted
+        if prompt := st.chat_input(placeholder = "Enter your prompt", 
+                                   accept_file = True, 
+                                   file_type = ["pdf", "png", "jpeg", "jpg"]) :
+            content = []
+            display_content = ""
 
-            for message in st.session_state.display_content:
-                with st.chat_message(message["role"]):
-                    st.markdown(message["content"], unsafe_allow_html = True)
+            if prompt.files:
+                file = prompt.files[0]
 
-            if prompt := st.chat_input(placeholder = "Enter your prompt", accept_file = True, file_type = ["pdf","png","jpeg","jpg"]):
-                content = []
-                display_content = ""
+                display_content += f"Uploaded {file.name}"
 
-
-            #content = []
-            #display_content = ""
-
-                if prompt.files:
-                    file = prompt.files[0]
-
-                    display_content += f"Uploaded {file.name}"
-
-                    # create pdf
-                    if file.type == "application/pdf":
-                        file = client.files.create(
-                            file = file, 
-                            purpose = "user_data"
-
-                        )
-
-                        content.append({"type":"file", "file":{"file_id":file.id}})
+                # create pdf
+                if file.type == "application/pdf":
+                    file = client.files.create(
+                        file = file,
+                        purpose = "user_data"
+                    )
+                    content.append({"type": "file", "file" : {"file_id": file.id}})
 
                 # create images
-                    else:
-                        base64_image = base64.b64encode(file.read()).decode("utf-8")
-                        image_url = f"data:image/jpeg;base64,{base64_image}"
+                else:
+                    base64_image = base64.b64encode(file.read()).decode("utf-8")
+                    image_url = f"data:image/jpeg;base64,{base64_image}"
 
-                        content.append({"type": "image_url", "image_url":{"url": image_url}})
+                    content.append({"type": "image_url", "image_url":{"url":image_url}})
                     
             
             if prompt.text and prompt.files: display_content += "<br>"
@@ -68,38 +58,26 @@ if api_key:
                 display_content += text
                 content.append({"type": "text", "text": text})
 
-            st.session_state.messages.append({"role": "user", "content": content})
-            st.session_state.display_content.append({"role": "user", "content":display_content})
-
             # store all history user content in session state
-
+            st.session_state.messages.append({"role": "user", "content": content})
+            st.session_state.display_content.append({"role": "user", "content": display_content})
             # display the user content 
-
-            # display the assistant content 
-
-            # store all history assistant content in session state
-
-            with st.chat_messsage("user"):
+            with st.chat_message("users"):
                 st.markdown(display_content, unsafe_allow_html = True)
-
+            # display the assistant content 
             with st.chat_message("assistant"):
                 stream = client.chat.completions.create(
                     model = model_name,
-                    message = [
+                    messages = [
                         {"role": m["role"], "content": m["content"]}
                         for m in st.session_state.messages
-
-
                     ],
-
                     stream = True,
-
                 )
-
                 response = st.write_stream(stream)
-
-            st.session_state.messages.append({"role":"assistant", "content": response})
-            st.session_state.display_content.append({"role": "assistant", "content":response})
+            # store all history assistant content in session state
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            st.session_state.display_content.append({"role": "assistant", "content": response})
 
     except Exception as e:
         st.info(e)
